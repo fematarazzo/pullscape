b99f43a1413b29282ceeaac4363f038b644b7b8a
Felipe Matarazzo <felipemps@protonmail.com>
2026-05-05 11:52:45 -0300

    github graphql client with upstream merge detection

---
+++ github.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const githubGraphQL = "https://api.github.com/graphql"
+
+const prQuery = `
+query GetUserPRs($username: String!, $first: Int!, $after: String) {
+  user(login: $username) {
+    pullRequests(first: $first, after: $after, orderBy: {field: CREATED_AT, direction: DESC}) {
+      pageInfo { hasNextPage endCursor }
+      nodes {
+        id number title state
+        createdAt mergedAt closedAt isDraft
+        repository {
+          name
+          owner { login }
+          stargazerCount
+        }
+        url
+        timelineItems(itemTypes: [CLOSED_EVENT], last: 1) {
+          nodes {
+            ... on ClosedEvent {
+              closer { __typename }
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+
+type rawPR struct {
+	ID        string  `json:"id"`
+	Number    int     `json:"number"`
+	Title     string  `json:"title"`
+	State     string  `json:"state"`
+	CreatedAt string  `json:"createdAt"`
+	MergedAt  *string `json:"mergedAt"`
+	ClosedAt  *string `json:"closedAt"`
+	IsDraft   bool    `json:"isDraft"`
+	Repository struct {
+		Name  string `json:"name"`
+		Owner struct {
+			Login string `json:"login"`
+		} `json:"owner"`
+		StargazerCount int `json:"stargazerCount"`
+	} `json:"repository"`
+	URL           string `json:"url"`
+	TimelineItems struct {
+		Nodes []struct {
+			Closer *struct {
+				Typename string `json:"__typename"`
+			} `json:"closer"`
+		} `json:"nodes"`
+	} `json:"timelineItems"`
+}
+
+type gqlResponse struct {
+	Data struct {
+		User *struct {
+			PullRequests struct {
+				PageInfo struct {
+					HasNextPage bool    `json:"hasNextPage"`
+					EndCursor   *string `json:"endCursor"`
+				} `json:"pageInfo"`
+				Nodes []rawPR `json:"nodes"`
+			} `json:"pullRequests"`
+		} `json:"user"`
+	} `json:"data"`
+	Errors []struct {
+		Message string `json:"message"`
+	} `json:"errors"`
+}
+
+func fetchAllPRs(token, username string) ([]rawPR, error) {
+	var all []rawPR
+	var cursor *string
+
+	for {
+		page, hasNext, next, err := fetchPage(token, username, cursor)
+		if err != nil {
+			return nil, err
+		}
+		all = append(all, page...)
+		if !hasNext {
+			break
+		}
+		cursor = next
+	}
+
+	return all, nil
+}
+
+func fetchPage(token, username string, after *string) ([]rawPR, bool, *string, error) {
+	body := map[string]any{
+		"query": prQuery,
+		"variables": map[string]any{
+			"username": username,
+			"first":    100,
+			"after":    after,
+		},
+	}
+
+	b, _ := json.Marshal(body)
+	req, _ := http.NewRequest("POST", githubGraphQL, bytes.NewReader(b))
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", "pullscape")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, false, nil, err
+	}
+	defer resp.Body.Close()
+
+	data, _ := io.ReadAll(resp.Body)
+
+	var result gqlResponse
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil, false, nil, err
+	}
+
+	if len(result.Errors) > 0 {
+		return nil, false, nil, fmt.Errorf("github: %s", result.Errors[0].Message)
+	}
+	if result.Data.User == nil {
+		return nil, false, nil, fmt.Errorf("user %q not found", username)
+	}
+
+	prs := result.Data.User.PullRequests
+	return prs.Nodes, prs.PageInfo.HasNextPage, prs.PageInfo.EndCursor, nil
+}
